9d1a0048ba9dbb7726bfd8120a8b7801161f6dad
HeyBroooo <mohitkumar4049a@gmail.com>
2023-08-26 13:25:40 +0530

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,45 @@
 import Link from "next/link";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faBars, faMale, faFemale, faShoePrints } from "@fortawesome/free-solid-svg-icons";
+import { IoMdHome, IoMdPerson } from "react-icons/io";
+
 import "../styles/navbaricon.css"
 
 const Navbar = () => {
   return (
-    <div className="block lg:block bg-black text-white p-4 ">
-      <div className="container">
-        <div className="flex w-fit gap-10 mx-auto font-small py-4 text-blackish">
-          <Link className="navbar__link relative flex items-center" href="#">
-            <div className="mr-2">
-              <FontAwesomeIcon icon={faHome} className="icon" />
-            </div>
-            HOME
+    <header className="text-gray-600 body-font">
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <Link legacyBehavior href="#">
+            <a className="mr-2 md:mr-10  icon-box">
+              <img src="/shoplocation.png" alt="Home" width="55" height="55" />
+            </a>
           </Link>
-          <Link className="navbar__link relative flex items-center" href="#">
-            <div className="mr-2">
-              <FontAwesomeIcon icon={faBars} className="icon" />
-            </div>
-            CATEGORIES
+          <Link legacyBehavior href="#">
+            <a className="mr-2 md:mr-10 icon-box">
+              <img src="/track.png" alt="Home" width="55" height="55" />
+            </a>
           </Link>
-          <Link className="navbar__link relative flex items-center" href="#">
-            <div className="mr-2">
-              <FontAwesomeIcon icon={faMale} className="icon"/>
-            </div>
-            {`MEN'S`}
+          <Link legacyBehavior href="#">
+            <a className="mr-2 md:mr-10 icon-box">
+              <img src="/sales.png" alt="Home" width="55" height="55" />
+            </a>
           </Link>
-          <Link className="navbar__link relative flex items-center" href="#">
-            <div className="mr-2">
-              <FontAwesomeIcon icon={faFemale} className="icon" />
-            </div>
-            {`WOMEN'S`}
+          <Link legacyBehavior href="#">
+            <a className="mr-2 md:mr-10 icon-box">
+              <img src="/bitcoin.png" alt="Home" width="55" height="55" />
+            </a>
           </Link>
-          <Link className="navbar__link relative flex items-center" href="#">
-            <div className="mr-2">
-              <FontAwesomeIcon icon={faShoePrints} className="icon" />
-            </div>
-            SHOES
+          <Link legacyBehavior href="#">
+            <a className="mr-2 md:mr-10 icon-box">
+              <img src="/sale.png" alt="Home" width="55" height="55" />
+            </a>
           </Link>
-        </div>
-      </div>
+        
+      </nav>
     </div>
-  );
-};
+ 
+  </header>
+  )
+}
 
 export default Navbar;
